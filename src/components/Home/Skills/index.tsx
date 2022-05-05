a144d65ba2aee98d24fd5ac3bb09e6d75f612a95
jinWoo_Lee <dlwlsdn201@naver.com>
2022-05-05 18:40:02 +0900

    [feature] 1. 스킬 숙련도 값 범위에 따른 색상 표현 기능 구현 + 2. 상태바 JSX 렌더링 방식 변경(하드코딩 -> 함수화) + 3. 스킬 언어 title 타입 형식 수정 업데이트(텍스트 -> 아이콘)

--- src/components/Home/Skills/index.tsx
+++ src/components/Home/Skills/index.tsx
@@ -1,9 +1,36 @@
 import React from 'react';
 import homeStore from '../../../resource/store/home';
 import { ProgressBar } from 'react-bootstrap';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import {
+	faCss3,
+	faHtml5,
+	faJs,
+	faReact,
+	faSass
+} from '@fortawesome/free-brands-svg-icons';
+import { ISkillStateObj } from '../../../resource/store/home';
+import { Tstatus } from '../../../types';
+
+function renderProgressBar(langauge: ISkillStateObj) {
+	let status: Tstatus = 'success';
+	const VALUE: number = langauge.value;
+	if (VALUE < 35) status = 'danger';
+	else if (VALUE < 70) status = 'warning';
+
+	return (
+		<ProgressBar
+			animated
+			role='progressbar'
+			now={langauge.value}
+			label={`${langauge.value}%`}
+			variant={status}
+		/>
+	);
+}
 
 const Skills = () => {
-	const { html, css, javascript, react } = homeStore();
+	const { html, css, javascript, react, sass } = homeStore();
 
 	return (
 		<div className='div_skills-inner'>
@@ -17,26 +44,47 @@ const Skills = () => {
 						</div>
 						<div className='div_skills-item-block'>
 							<div className='div_skills-icon'>
-								<strong>html</strong>
+								<FontAwesomeIcon
+									icon={faHtml5}
+									size={'2x'}
+									color={html.color}
+								/>
+							</div>
+							<div className='div_skills-level'>{renderProgressBar(html)}</div>
+						</div>
+						<div className='div_skills-item-block'>
+							<div className='div_skills-icon'>
+								<FontAwesomeIcon icon={faCss3} size={'2x'} color={css.color} />
+							</div>
+							<div className='div_skills-level'>{renderProgressBar(css)}</div>
+						</div>
+						<div className='div_skills-item-block'>
+							<div className='div_skills-icon'>
+								<FontAwesomeIcon icon={faSass} size={'2x'} color={sass.color} />
+							</div>
+							<div className='div_skills-level'>{renderProgressBar(sass)}</div>
+						</div>
+						<div className='div_skills-item-block'>
+							<div className='div_skills-icon'>
+								<FontAwesomeIcon
+									icon={faJs}
+									size={'2x'}
+									color={javascript.color}
+								/>
 							</div>
 							<div className='div_skills-level'>
-								<ProgressBar now={html} label={`${html}%`} color="orange"/>
+								{renderProgressBar(javascript)}
 							</div>
 						</div>
 						<div className='div_skills-item-block'>
-							<div className='div_skills-icon'>css</div>
-							<div className='div_skills-level'>
-								<ProgressBar now={css} label={`${css}%`} color="blue"/></div>
-						</div>
-						<div className='div_skills-item-block'>
-							<div className='div_skills-icon'>JavaScript</div>
-							<div className='div_skills-level'>
-								<ProgressBar now={javascript} label={`${javascript}%`} color="yellow"/></div>
-						</div>
-						<div className='div_skills-item-block'>
-							<div className='div_skills-icon'>React</div>
-							<div className='div_skills-level'>
-								<ProgressBar now={react} label={`${react}%`} /></div>
+							<div className='div_skills-icon'>
+								<FontAwesomeIcon
+									icon={faReact}
+									size={'2x'}
+									color={react.color}
+								/>
+							</div>
+							<div className='div_skills-level'>{renderProgressBar(react)}</div>
 						</div>
 					</div>
 				</div>
