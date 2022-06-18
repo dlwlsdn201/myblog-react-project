8131d500fb9b4512f1431dbe5093504bbbde1ad8
jinWoo_Lee <dlwlsdn201@naver.com>
2022-06-18 13:04:50 +0900

    [home][fix] 1. 태블릿, 모바일 스크린일 때, 폰트 사이즈 스타일 이슈 수정

--- src/components/Home/index.tsx
+++ src/components/Home/index.tsx
@@ -11,13 +11,9 @@ const HomeContainer = () => {
 			<div className='p-4 p-md-5 mb-4 text-white rounded bg-dark intro-card'>
 				<section className='section_background'>
 					<div className='col-md-6 px-0'>
-						<h1 className='display-4 fst-italic'>게으른 완벽주의 INFJ</h1>
+						<h1 className='display-4 fst-italic'>게으른 완벽주의 ESFJ</h1>
 						<p className='lead my-3'>개발 중</p>
-						<p className='lead mb-0'>
-							<a href='#' className='text-white fw-bold'>
-								반갑습니다
-							</a>
-						</p>
+						<p className='lead mb-0'></p>
 					</div>
 				</section>
 			</div>
